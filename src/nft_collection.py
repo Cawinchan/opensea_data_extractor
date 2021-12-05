6d8ec9815b4f2f8656e79669b7e784c1c8cbcfbe
cawinchan <cawin.chan@gmail.com>
2021-12-05 20:45:40 +0800

    Update VIdeo Duration visualisations

--- src/nft_collection.py
+++ src/nft_collection.py
@@ -16,17 +16,17 @@ collection = {
     "boonjiproject": "0x4cd0ea8b1bdb5ab9249d96ccf3d8a0d3ada2bc76",           # (Image: JPG)       [Art, Rank #4, last 7 days]
     "fidenza-by-tyler-hobbs": "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",  # (Image: JPG)       [Art, Rank #5, last 7 days]
     "lostpoets": "0x4b3406a41399c7fd2ba65cbc93697ad9e7ea61e5",               # (Image: JPG)       [Art, Rank #8, last 7 days]
-    "theshiboshis": "0x11450058d796b02eb53e65374be59cff65d3fe7f",            # (Image: JPG) 
-    "veefriends":"0xa3aee8bce55beea1951ef834b99f3ac60d1abeeb",               # (Image: JPG)
+    "theshiboshis": "0x11450058d796b02eb53e65374be59cff65d3fe7f",            # (Image: JPG)       [Art]
+    "veefriends":"0xa3aee8bce55beea1951ef834b99f3ac60d1abeeb",               # (Image: JPG)       [Art]
     "song-a-day": "0x495f947276749ce646f68ac8c248420045cb7b5e",              # (Video: MP4)       [Music, Rank #2, last 7 days]
     "eulerbeats-enigma": "0xa98771a46dcb34b34cdad5355718f8a97c8e603e",       # (Music: MP4)       [Music, Rank #3, last 7 days]
-    "eulerbeats":"0x8754f54074400ce745a7ceddc928fb1b7e985ed6",               # (Music: MP4) 
+    "eulerbeats":"0x8754f54074400ce745a7ceddc928fb1b7e985ed6",               # (Music: MP4)       [Music, Rank #1, all time]
     "spottiewifi": "0xa0e1b198bcc877a950a29512ab5c0ce1bb964c97",             # (Music: MP4)       [Music, Rank #8, last 7 days]
-    "namewee4896-collection": "0x495f947276749ce646f68ac8c248420045cb7b5e",  # (Music: MP3 + JPG) 
-    "jinglebe-nft-collection": "0x9951f5da4f4f21b7d39a80bc665edf31bd515009", # (Music)            [Music, Rank #4, all time]
-    "async-music":"0x3e43944d977dea22511da6d33c0cab666a604515",              # (Music: MP3 + JPG) [Music, Rank #7, all time]
-    "niftysaxspheres":"0xf89cfa360a2b883d7325d94eeea89c2b7079c1a3",          # (Music: MP4)       [Music, Rank #9, last 7 days]
-    "calvin-harris-x-emil-nava":"0xbe7e0d467d9e0235c8ac5a7d68e55e198ff2235b",# (Music: MP4)       [Music, Rank #7, all time]
+    "namewee4896-collection": "0x495f947276749ce646f68ac8c248420045cb7b5e",  # (Music: MP3 + JPG) [Music, Rank #4, all time]
+    "jinglebe-nft-collection": "0x9951f5da4f4f21b7d39a80bc665edf31bd515009", # (Music)            [Music, Rank #5, all time]
+    "async-music":"0x3e43944d977dea22511da6d33c0cab666a604515",              # (Music: MP3 + JPG) [Music, Rank #9, all time]
+    "niftysaxspheres":"0xf89cfa360a2b883d7325d94eeea89c2b7079c1a3",          # (Music: MP4)       [Music]
+    "calvin-harris-x-emil-nava":"0xbe7e0d467d9e0235c8ac5a7d68e55e198ff2235b",# (Music: MP4)       [Music]
     "superrare": "0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0",               # (Image: JPG | GIF: GIF | Audio: MP3 | Video: MP4) Marketplaces
     "rarible":"0xd07dc4262bcdbf85190c01c996b4c06a461d2430"                   # (Image: JPG | GIF: GIF | Audio: MP3 | Video: MP4) Marketplaces
 }
